eca029238dcabcf9e304f4b0475746b330103fb4
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-11-29 00:26:38 +0900

    テーブルを追加

--- check/admin.py
+++ check/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import CompanyModel, CompanyHomepageURLModel, EsModel
+from .models import CompanyModel, CompanyHomepageURLModel, EsModel, TagModel, CustomUserModel, QestionModel
 
 # Register your models here.
 admin.site.register(CompanyModel)
 admin.site.register(CompanyHomepageURLModel)
 admin.site.register(EsModel)
+admin.site.register(CustomUserModel)
+admin.site.register(TagModel)
+admin.site.register(QestionModel)
--- check/models.py
+++ check/models.py
@@ -1,6 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUserModel(AbstractUser):
+    '''カスタムユーザークラス'''
+    class Meta(object):
+        db_table = 'custom_user'
+
+    def __str__(self):
+        return self.username
+
+
+class TagModel(models.Model):
+    tag_name = models.CharField(max_length=50)
+    author = models.ManyToManyField(CustomUserModel)
+
+    def __str__(self):
+        return self.tag_name
 
 
 class CompanyModel(models.Model):
@@ -22,10 +40,18 @@ class EsModel(models.Model):
     company = models.ForeignKey(CompanyModel, on_delete=models.CASCADE)
     company_url = models.ForeignKey(CompanyHomepageURLModel, on_delete=models.CASCADE)
     selection_type = models.CharField(max_length=100, null=True, blank=True)
-    print(company)
 
     def __str__(self):
         if self.selection_type is None:
             return self.company.company_name + '_none'
         else:
             return self.company.company_name + '_' + self.selection_type
+
+
+class QestionModel(models.Model):
+    es = models.ForeignKey(EsModel, on_delete=models.CASCADE)            
+    question = models.TextField()
+    tags = models.ManyToManyField(TagModel)
+
+    def __str__(self):
+        return self.question
